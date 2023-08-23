99a07ed4481d8c6aae462a00fdbe28248f3af757
bhavens <bmhavens6@gmail.com>
2023-08-23 16:31:35 -0500

    beginning force detection algorithm for force sensor

---
+++ Src/robot/force_detector.py
@@ -0,0 +1,13 @@
+from serial import Serial
+
+'''
+
+    Force detection will use an anomaly detection algorithm to detect sudden changes in the force sensor which 
+    will indicate a possible collision.
+
+'''
+
+class ForceDetector:
+
+    def __init__(self):
+        print("Hello world")
